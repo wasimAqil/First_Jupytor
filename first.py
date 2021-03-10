9975b50f9843202b82842deca1cfb04b0db77d08
wasimAqil <78753265+wasimAqil@users.noreply.github.com>
2021-03-09 16:47:12 -0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#   
+#   #  Introduction
+# 
+
+#   This is my   *first*   Jupyter Notebook
+# 
+
+# In[9]:
+
+
+x= 5
+y= 6
+z = x + y
+
+print('sum is ',z)
+
+
+# In[4]:
+
+
+
+
